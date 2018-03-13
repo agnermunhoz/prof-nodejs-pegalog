a5f04a21e79c79d696b83010c05677471cb25dd5
Agner Munhoz <agner.munhoz@sulamerica.com.br>
2018-03-13 09:55:40 -0300

    Bootstrap nas views

--- controllers/logs.js
+++ controllers/logs.js
@@ -2,7 +2,7 @@ module.exports = function (app) {
 
     var fs = require('fs');
     var url = require('url');
-    
+
     var LogsController = {
         view: function (request, response) {
             var usuario = request.session.usuario;
@@ -11,10 +11,17 @@ module.exports = function (app) {
             if (!q.search) {
                 console.log("Arquivo não encotrado.");
                 response.status(404).send("Not found");
-            } 
+            }
             else {
-                var filePath = appConfig.basePath + decodeURI(q.search.substr(1));
-                response.download(filePath);
+                var path = decodeURI(q.search.substr(1));
+                if (path.indexOf("..") > -1) {
+                    console.log("Navegação proibida.");
+                    response.status(403).send("Forbidden");
+                }
+                else {
+                    var filePath = appConfig.basePath + decodeURI(path);
+                    response.download(filePath);
+                }
             }
         },
         browser: function (request, response) {
@@ -26,12 +33,18 @@ module.exports = function (app) {
                 path = decodeURI(q.search.substr(1));
             }
 
-            var dirs = fs.readdirSync(appConfig.basePath+path).filter(f => fs.statSync(appConfig.basePath+path+f).isDirectory());
-            var files = fs.readdirSync(appConfig.basePath+path).filter(f => fs.statSync(appConfig.basePath+path+f).isFile());
-            
-            var params = { usuario: usuario, path: path, dirs: dirs, files: files};
-            response.render('logs/browser', params);
-            
+            if (path.indexOf("..") > -1) {
+                console.log("Navegação proibida. Path '"+path+"'");
+                response.status(403).send("Forbidden");
+            }
+            else {
+
+                var dirs = fs.readdirSync(appConfig.basePath + path).filter(f => fs.statSync(appConfig.basePath + path + f).isDirectory());
+                var files = fs.readdirSync(appConfig.basePath + path).filter(f => fs.statSync(appConfig.basePath + path + f).isFile());
+
+                var params = { usuario: usuario, path: path, dirs: dirs, files: files };
+                response.render('logs/browser', params);
+            }
         }
     };
 
